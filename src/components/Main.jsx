e4152addf81a63068e1d163a82fc9fb2f902a68a
Carlos Hernandez <carloshernandezp21@gmail.com>
2024-06-30 16:15:15 +0200

    cambiando la ruta de la imagen x2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,7 +18,7 @@ export const Main = () => {
 
       <section id='aboutSection'>
         <div>
-          <img src='../img/CodeTyping.png' />
+          <img src='../src/img/CodeTyping.png' />
           <h2>About <span className='highlight'>Me</span></h2>
           <p>I am a self-taught <span className='highlight'>Frontend Junior Developer</span> passionated about creating interactive and engaging web experiences.</p>
           <p>With solid knowledge of HTML, CSS and JAVASCRIPT. Seeking for new oportunities to grow professionally and contribute my creativity and technic skills in a collaborative enviroment.</p>
